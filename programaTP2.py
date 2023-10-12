009a934ac8524f398234ee570dc5b53fb89d8923
NicoAgHerrera <nicagustin2010@gmail.com>
2023-10-12 11:57:43 -0300

    A y B

--- programaTP2.py
+++ programaTP2.py
@@ -1,6 +1,6 @@
 import numpy as np
-
-def genera_palabras_codigo(nombre_archivo):
+import sys
+def cuenta_palabras_codigo(nombre_archivo):
     palabras_codigo={}
     with open(nombre_archivo,"rt") as archivo: #se abre el archivo en modo de lectura binaria
         for linea in archivo:     # reading each line 
@@ -13,27 +13,24 @@ def genera_palabras_codigo(nombre_archivo):
     archivo.close()
     return palabras_codigo
 
-def genera_probabilidades_palabras_codigo(palabras_codigo):
-    total_palabras=0
-    for palabra in palabras_codigo:
-        total_palabras+=palabras_codigo[palabra]
-    probabilidades_palabras_codigo={}
-    for palabra in palabras_codigo:
-        probabilidades_palabras_codigo[palabra]=palabras_codigo[palabra]/total_palabras
-    return probabilidades_palabras_codigo
+def probabilidad_palabras(palabras_codigo):
+    total= sum(palabras_codigo.values())
+    if(total!=0):
+        for palabra in palabras_codigo:
+            palabras_codigo[palabra]/=total
     
-def genera_alfabeto_codigo(probabilidades_palabras_codigo):
-    alfabeto_codigo=[]
-    for palabra in probabilidades_palabras_codigo:
-        for letra in palabra:
-            if letra not in alfabeto_codigo:
-                alfabeto_codigo.append(letra)
-    alfabeto_codigo.sort()
-    return alfabeto_codigo
+def genera_alfabeto_codigo(palabras_codigo):
+    alfabeto = set()  # Usamos un conjunto para asegurar caracteres únicos
+
+    for palabra in palabras_codigo:
+        alfabeto.update(set(palabra))
+
+    return sorted(list(alfabeto))  # Convierte el conjunto a una lista ordenada
+
 
 def genera_probabilidades_simbolos(palabras_codigo,alfabeto_codigo):
     total_simbolos=0
-    probabilidades_simbolos={for letra in alfabeto_codigo :0}
+#    probabilidades_simbolos={for letra in alfabeto_codigo :0}
     for palabra in palabras_codigo:
         for letra in palabra:
             if letra in alfabeto_codigo:
@@ -44,15 +41,18 @@ def genera_probabilidades_simbolos(palabras_codigo,alfabeto_codigo):
         probabilidades_simbolos/=total_simbolos
     return probabilidades_simbolos
 
-palabras_codigo=genera_palabras_codigo(r"tp2_sample0.txt") #Es diccionario
-probabilidades_palabras_codigo=genera_probabilidades_palabras_codigo(palabras_codigo) #Es diccionario
-print(probabilidades_palabras_codigo)
-alfabeto_codigo=genera_alfabeto_codigo(probabilidades_palabras_codigo)
-print(alfabeto_codigo)
-probabilidades_simbolos=genera_probabilidades_simbolos(palabras_codigo,alfabeto_codigo)
-print(probabilidades_simbolos)
-#if len(sys.argv) >1:
-#   filename = sys.argv[1]
-#    mat_acum=Genera_Matriz_Acumulada(filename)
-#else:
-#    print("No se proporcionaron parámetros.")
+
+
+#probabilidades_simbolos=genera_probabilidades_simbolos(palabras_codigo,alfabeto_codigo)
+#print(probabilidades_simbolos)
+
+if len(sys.argv) ==2:
+    palabras_codigo= cuenta_palabras_codigo(sys.argv[1])
+    probabilidad_palabras(palabras_codigo)
+    print("Las probabilidades de las palabras contenidas en el archivo son:")
+    for palabra, probabilidad in palabras_codigo.items():
+        print(f"{palabra}: {probabilidad:.2f}")
+    alfabeto_codigo= genera_alfabeto_codigo(palabras_codigo)
+    print("El alfabeto resulta: ",alfabeto_codigo)
+else:
+    print("Error: no se ha ingresado el nombre del archivo de texto")
