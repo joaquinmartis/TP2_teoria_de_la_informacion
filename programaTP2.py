8d8f53957068482cfc0650858f2ad1e50c62031c
NicoAgHerrera <nicagustin2010@gmail.com>
2023-10-12 19:06:42 -0300

    Fin :)

--- programaTP2.py
+++ programaTP2.py
@@ -15,12 +15,11 @@ def cuenta_palabras_codigo(nombre_archivo):
     return palabras_codigo
 
 def genera_probabilidades_palabras_codigo(palabras_codigo):
-    total_palabras=0
-    for palabra in palabras_codigo:
-        total_palabras+=palabras_codigo[palabra]
-    probabilidades_palabras_codigo={}
-    for palabra in palabras_codigo:
-        probabilidades_palabras_codigo[palabra]=palabras_codigo[palabra]/total_palabras
+    total_palabras=sum(palabras_codigo.values())
+    if(total_palabras!=0):
+        probabilidades_palabras_codigo={}
+        for palabra in palabras_codigo:
+            probabilidades_palabras_codigo[palabra]=palabras_codigo[palabra]/total_palabras
     return probabilidades_palabras_codigo   
     
 def genera_alfabeto_codigo(palabras_codigo):
@@ -30,20 +29,6 @@ def genera_alfabeto_codigo(palabras_codigo):
         alfabeto.update(set(palabra))
 
     return sorted(list(alfabeto))  # Convierte el conjunto a una lista ordenada
-
-
-def genera_probabilidades_simbolos(palabras_codigo,alfabeto_codigo):
-    total_simbolos=0
-#    probabilidades_simbolos={for letra in alfabeto_codigo :0}
-    for palabra in palabras_codigo:
-        for letra in palabra:
-            if letra in alfabeto_codigo:
-                probabilidades_simbolos[letra]+=palabras_codigo[palabra]
-    for letra in probabilidades_simbolos:
-        total_simbolos+=probabilidades_simbolos[letra]
-    for letra in probabilidades_simbolos:
-        probabilidades_simbolos/=total_simbolos
-    return probabilidades_simbolos
 
 def calcular_entropia (palabras_codigo, cant_simbolos):
     entropia = 0
@@ -61,12 +46,12 @@ def calcular_longitud_media(probabiliad_palabras):
 def KraftyMcMillan(palabras_codigo,cant_simbolos):
     K=0
     for palabra in palabras_codigo.items():
-        K+= cant_simbolos ** (-len(palabra))
+        K+= cant_simbolos ** -len(palabra[0])
     return K<=1
 
 def is_codigo_compacto(palabras_codigo,cant_simbolos):
     for palabra,probabilidad in palabras_codigo.items():
-        if(len(palabra)<= round(math.log(1/probabilidad)/math.log(cant_simbolos))):
+        if(len(palabra)> round(math.log(1/probabilidad)/math.log(cant_simbolos))):
             return False
     return True
 
@@ -82,7 +67,7 @@ if len(sys.argv) ==2:
     probabilidad_palabras=genera_probabilidades_palabras_codigo(palabras_codigo)
     print("Las probabilidades de las palabras contenidas en el archivo son:")
     for palabra, probabilidad in probabilidad_palabras.items():
-        print(f"Palabra: {palabra} = {probabilidad:.2f}")
+        print(f"Palabra: {palabra} = {probabilidad:.4f}")
     alfabeto_codigo= genera_alfabeto_codigo(probabilidad_palabras)
     print("El alfabeto resulta: ",alfabeto_codigo)
     entropia=calcular_entropia(probabilidad_palabras,len(alfabeto_codigo))
@@ -93,10 +78,14 @@ if len(sys.argv) ==2:
         print("La codificación cumple con las inecuaciones de Kraft y McMillan")
         if is_codigo_instantaneo(palabras_codigo):
             print("Las longitudes de las palabras constituyen un codigo instantaneo")
+        else:
+            print("No es instantaneo")
     else:
-        print("La codificación NO cumple con las inecuaciones de Kraft y McMillan ")
+        print("La codificación NO cumple con las inecuaciones de Kraft y McMillan y por eso no es instantaneo")
     if is_codigo_compacto(probabilidad_palabras,len(alfabeto_codigo)):
         print("El codigo es compacto")
+    else:
+        print("No es compacto")
   
 else:
     print("Error: no se ha ingresado el nombre del archivo de texto")
